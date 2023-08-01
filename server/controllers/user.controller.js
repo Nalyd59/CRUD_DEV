f1f31c86541c44f4eb2dcfca3525997cd0db89c7
Nalyd59 <dy.crepin@gmail.com>
2023-08-01 13:32:05 +0200

    first commit

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,77 @@
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+})
+
+// User
+const createUser = (req, res) => {
+  // utilise req.body de body-parser
+  const { nom, prenom, mail, adresse, ville, codepostal, telephone } = req.body;
+  // vérifier si les champs sont remplis
+  if (!nom || !prenom || !mail || !adresse || !ville || !codepostal || !telephone) {
+    return res.status(400).json({
+      error: 'donnée manquante',
+    })
+  }
+  const query = 'INSERT INTO users (nom, prenom, mail, adresse, ville, codepostal, telephone) VALUES (?,?,?,?,?,?,?)';
+  conn.query(query, [nom, prenom, mail, adresse, ville, codepostal, telephone], (err) => {
+    if (err) {
+      console.error('erreur lors de l\'insertion des données : ' + err);
+      res.status(500).json({ error: 'erreur lors de l\'insertion des données' });
+    } else {
+      res.status(200).json({ message: 'utilisateur enregistré' });
+    }
+  });
+};
+
+const updateUser = (req, res) => {
+  const { nom, prenom, mail, adresse, ville, codepostal, telephone } = req.body;
+  const query = 'UPDATE users SET nom = ?, prenom = ?, mail = ?, adresse = ?, ville = ?, codepostal = ?, telephone = ? WHERE id = ?';
+  conn.query(query, [nom, prenom, mail, adresse, ville, codepostal, telephone, req.params.id], (err) => {
+    if (err) {
+      console.error('Erreur lors de la modification de l\'utilisateur: ' + err);
+      res.status(500).json({
+        error: 'Erreur lors de la modification de l\'utilisateur'
+      });
+    } else {
+      console.log(res);
+      res.status(200).json({ message: 'Utilisateur modifié' });
+    }
+  });
+};
+
+const deleteUser = (req, res) => {
+  const query = 'DELETE FROM users WHERE id =?'
+  conn.query(query, [req.params.id], (err, result) => {
+    if (err) {
+      console.error('Erreur lors de la suppression des données : ' + err);
+      res.status(500).json({ error: 'Erreur lors de la suppression des données' });
+    } else {
+      res.status(200).json({ message: 'Utilisateur supprimé' });
+    }
+  })
+};
+
+const getAllUsers = (req, res) => {
+  const query = 'SELECT * FROM users';
+  conn.query(query, (err, result) => {
+    if (err) {
+      console.error('Erreur de la récupération des données ' + err);
+      res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+    } else {
+      res.status(200).json(result);
+    }
+  }
+
+  )
+}
+
+module.exports = {
+  createUser,
+  updateUser,
+  deleteUser,
+  getAllUsers,
+};
---
+++ server/server.js
@@ -0,0 +1,69 @@
+// Importation des dépendances express, body-parser et cors
+// Nouvelle version 
+// import express from "express";
+// Ancienne version
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user.route');
+const cors = require('cors');
+
+// Connection a la base de données
+
+const connectDb = require('./config/db');
+
+// Middlewares
+const app = express();
+
+// Use app Express
+app.use(express.json());
+
+// Use bodyParser
+app.use(bodyParser.json());
+// Permet de décripter des URL
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Use cors
+app.use(cors({
+    origin: 'http://127.0.0.1:8080',
+    optionsSuccessStatus: 200
+})
+);
+
+// Ecrire bonjour dans le navigation
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// })
+
+// Routes
+app.use('/', userRoutes);
+
+
+// Configuration et lancement du serveur
+const start = async () => {
+    try {
+        await connectDb();
+        const port = process.env.PORT || 5000;
+        app.listen(port, () => {
+            console.log(`le serveur à démarré sur le port ${port}`);
+
+        })
+    } catch (error) {
+        console.log(`Erreur lors du démarrage du serveur`);
+    }
+};
+
+start();
+
+
+
+
+
+
+
+
+
+
+
+
+
