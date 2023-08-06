f7abe70effac80cbcde12817a7b270b26fdc2bb1
Nalyd59 <dy.crepin@gmail.com>
2023-08-06 15:38:24 +0200

    css+reload

--- client/script.js
+++ client/script.js
@@ -1,33 +1,45 @@
+function handleDelete(id) {
+            fetch(`http://localhost:5000/${id}`,{
+            method: "DELETE",
+        });
+        document.location.reload()
+    }
+
 window.onload =  function () {
 
-    const modifUser = {
-        nom : "Alexandra",//document.getElementById('nom').value,
-        prenom : "Palombino",//document.getElementById('prenom').value,
-        mail : "minibino@gmail.com",//document.getElementById('mail').value,
-        adresse : "11 rue foch",//.getElementById('adresse').value,
-        ville : "Paris",//document.getElementById('ville').value,
-        codepostal : "75000",//document.getElementById('code_postal').value,
-        telephone : "0785253641"//document.getElementById('telephone').value
-    }
+    // const modifUser = {
+    //     nom : "Alexandra",//document.getElementById('nom').value,
+    //     prenom : "Palombino",//document.getElementById('prenom').value,
+    //     mail : "minibino@gmail.com",//document.getElementById('mail').value,
+    //     adresse : "11 rue foch",//.getElementById('adresse').value,
+    //     ville : "Paris",//document.getElementById('ville').value,
+    //     codepostal : "75000",//document.getElementById('code_postal').value,
+    //     telephone : "0785253641"//document.getElementById('telephone').value
+    // }
 
-    const newUser = {
-        nom : "hhhh",//document.getElementById('nom').value,
-        prenom : "hhhh",//document.getElementById('prenom').value,
-        mail : "a.dumortier@gmail.com",//document.getElementById('mail').value,
-        adresse : "55 rue sapin vert",//.getElementById('adresse').value,
-        ville : "hhh",//document.getElementById('ville').value,
-        codepostal : "13000",//document.getElementById('code_postal').value,
-        telephone : "0689641234"//document.getElementById('telephone').value
-    }
+    // const newUser = {
+    //     nom : "hhhh",//document.getElementById('nom').value,
+    //     prenom : "hhhh",//document.getElementById('prenom').value,
+    //     mail : "a.dumortier@gmail.com",//document.getElementById('mail').value,
+    //     adresse : "55 rue sapin vert",//.getElementById('adresse').value,
+    //     ville : "hhh",//document.getElementById('ville').value,
+    //     codepostal : "13000",//document.getElementById('code_postal').value,
+    //     telephone : "0689641234"//document.getElementById('telephone').value
+    // }
+
+    
 
     // Afficher les users
+
     fetch("http://localhost:5000/all",{
         method: "GET",
         headers: {
             "Content-type": "application/json"
         }
     })
+
     .then(response => response.json())
+
     .then(data => handleUsers(data))
 
     function handleUsers(data) {
@@ -50,26 +62,30 @@ window.onload =  function () {
 
 
     // Modifier un user
-    fetch("http://localhost:5000/4",{
-        method: "PUT",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify(modifUser)
-    })
-    .then(response => response.json())
+    
+    // fetch("http://localhost:5000/4",{
+    //     method: "PUT",
+    //     headers: {
+    //         "Content-type": "application/json"
+    //     },
+    //     body: JSON.stringify(modifUser)
+    // })
+    // .then(response => response.json())
 
 
     // Creer un user
+
     var btnAddUser = document.getElementById('btnAddUser');
+
     let form = document.getElementById('addUserForm');
+
     btnAddUser.addEventListener("click", () => {
         openForm();
     });
 
-    
-
-
+    btnAddUser.addEventListener("click", () => {
+        handleFormAdd();
+    });
 
     function handleFormAdd(e) {
         e.preventDefault();
@@ -98,36 +114,16 @@ window.onload =  function () {
             .then(response => response.text())
             .then(result => console.log(result))
             .catch(error => console.log('error', error));
-        openForm();
 
+        openForm();
     }
 
     form.addEventListener('submit', handleFormAdd);
 
-
-
     function openForm() {
         let form = document.getElementById('addUserForm');
         form.classList.toggle('hidden');
+        document.window.reload()
     }
-
     
-
-        
-        
-    }
-
-
-    // Supprimer un user
-    let btnDeleteUser = document.getElementById('btnDelete')
-    btnDeleteUser.addEventListener('click', handleDelete());
-
-    function handleDelete(id) {
-        
-        fetch(`http://localhost:5000/${id}`,{
-        method: "DELETE",
-    });
-
-     
-        
-    }
+}
